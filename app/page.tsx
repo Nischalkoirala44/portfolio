19d02f81e9da8477b0ad2266f79de0187a5a22bc
Nischalkoirala44 <davidkoirala66@gmail.com>
2025-06-26 01:18:02 +0545

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import HeroSection from './components/HeroSection'
 import Header from './components/Header'
 import About from "./components/About"
 import Project from "./components/Project"
-import Contact from "./components/Contact"
 import Skills from './components/Skills'
 
 const page = () => {
@@ -18,7 +17,6 @@ const page = () => {
       <Skills />
       <Project />
       <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-        <Contact />
       </div>
     </main>
     
